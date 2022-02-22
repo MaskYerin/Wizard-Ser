14cdd29cd180d9a949613d2796430f1e9379f343
TOXIC ALIEN <64305844+Alien-alfa@users.noreply.github.com>
2022-02-22 20:05:19 +0530

    Update lirik.js

--- plugins/lirik.js
+++ plugins/lirik.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text, usedPrefix, command }) => {
-  if (!text) throw `uhm.. what are you looking for?\n\nexample:\n${usedPrefix + command} shape of you`
+  if (!text) throw `Which Music lyrics do you want??\n\nexample:\n${usedPrefix + command} shape of you`
   let res = await fetch(global.API('bx', '/api/music/liriklagu', { query: text }, 'apikey'))
   if (!res.ok) throw await `${res.status} ${res.statusText}`
   let json = await res.json()
@@ -9,6 +9,6 @@ let handler = async (m, { text, usedPrefix, command }) => {
 }
 handler.help = ['lyric'].map(v => v + ' <song>')
 handler.tags = ['internet']
-handler.command = /^(lyric|lyrics?)$/i
+handler.command = /^(lyric|ly|lyrics?)$/i
 
 module.exports = handler
